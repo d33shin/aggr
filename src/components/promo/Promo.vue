3c26e66e38c124d521d82b3150f954b5fdd583f2
David Shin <d33shin@gmail.com>
2024-03-22 22:14:01 +0900

    saved

--- src/components/promo/Promo.vue
+++ src/components/promo/Promo.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    <a class="discord-link" href="http://discord.com/invite/XZ6fJTFNx7">
+    <a
+      class="discord-link"
+      href="http://discord.com/invite/XZ6fJTFNx7"
+      target="_blank"
+    >
       국내 최대<br />
       선물 커뮤니티<br />코인톡 바로가기</a
     >
-    <a href="https://discord.com/invite/tWE7waTAnP">거래소 할인 최대 50% </a>
+    <a href="https://discord.com/invite/tWE7waTAnP" target="_blank"
+      >거래소 할인 최대 50%
+    </a>
 
-    <a href="https://discord.com/invite/3DxttevJCG">거래소 1:1문의</a>
+    <a href="https://discord.com/invite/3DxttevJCG" target="_blank"
+      >거래소 1:1문의</a
+    >
     <hr />
-    <a href="https://coincast.kr">실시간 비트코인 뉴스</a>
+    <a href="https://coincast.kr" target="_blank">실시간 비트코인 뉴스</a>
   </div>
 </template>
 
